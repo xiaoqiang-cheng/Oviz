49475019fb62c1c4cb284eea1e302e8b51a12ffc
xiaoqiang cheng <xiaoqiang.cheng@foxmail.com>
2023-10-07 21:31:48 +0800

    support windows plugin path

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from Controller.controller import Controller
 import signal
 from log_sys import _log_sys_init
 from Utils.common_utils import *
+import platform
 
+system_platform = platform.platform()
 
 dirname = os.path.dirname(GuiCoreLib.__file__)
-plugin_path = os.path.join(dirname, "Qt",  'plugins', 'platforms')
+if "Windows" in system_platform:
+    plugin_path = os.path.join(dirname,'plugins', 'platforms')
+elif "Linux" in system_platform:
+    plugin_path = os.path.join(dirname,'Qt', 'plugins', 'platforms')
+
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
 
 def main():
     _log_sys_init()
